36d4a2f7dbc2090b47701da2202895894c8b049c
Skypea-Ghoul <apirafly12@gmail.com>
2025-03-14 20:38:49 +0700

    Initial Codes

--- app/src/main/java/com/example/myapplication/ui/theme/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 
-const val BASE_URL = "http://10.0.2.2:8000"
+const val BASE_URL = "http://127.0.0.1:8000"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
